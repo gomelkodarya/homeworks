b2bbd43ec6b28a504031a27d14f71b72e92d439e
gomelkodarya <gomelkodarya@gmail.com>
2021-09-20 15:46:50 +0300

    modify function onChangeCallback

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -4,7 +4,7 @@ import s from './SuperInputText.module.css'
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
-// здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
+// здесь мы 0говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
     onChangeText?: (value: string) => void
