a2e35aa95ad4440ed71b8bfb741c1376382d8c91
gomelkodarya <gomelkodarya@gmail.com>
2022-02-04 15:58:25 +0300

    add Homework 9

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -5,6 +5,7 @@ import HW3 from '../../h3/HW3'
 import HW4 from '../../h4/HW4'
 import HW7 from "../../h7/HW7";
 import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 function Junior() {
     return (
@@ -12,7 +13,7 @@ function Junior() {
             junior
             <HW7/>
             <HW8/>
-            {/*<HW9/>*/}
+            <HW9/>
             {/*<HW10/>*/}
             {/*<HW11/>*/}
         </div>
